b340ca90030087e23b59966f2759128f3014324a
najib <ikniounnajib@gmail.com>
2025-09-25 11:16:57 +0100

    fonctions

--- challenge4/recherche.js
+++ challenge4/recherche.js
@@ -1,1 +1,17 @@
 const prompt = require("prompt-sync")()
+let a = []
+let i = 0 
+let b
+while (i < 3){
+    a[i] = prompt("entre un mot ")
+    i++
+}
+console.log(a)
+b= prompt("mot a la recherche ")
+
+if (!(a.includes(b))){
+    console.log("nom pas trouve")
+}
+else{
+    console.log("le nom" + b+ "a ete trouver a la position "(a.indexof(b)+1)) 
+}
